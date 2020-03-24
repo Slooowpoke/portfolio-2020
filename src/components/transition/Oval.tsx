062d4acfa6e876ed0d4ddb88c563e6f9113b21bf
Mali Coxon <mali@expend.io>
2020-03-24 20:40:40 +0000

    feat: Added basic transition animation

---
+++ src/components/transition/Oval.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledOval = styled.div`
+    position: fixed;
+    top: ${props => `calc(20% - (${props.initialHeight}vh / 4))`};
+    border: 2px solid #fff;
+    width: ${props => `${props.initialWidth}vw`};
+    height: ${props => `${props.initialHeight}vh`};
+    border-radius: 100%;
+    left: 50%;
+    transform: translateX(-50%);
+    opacity: 0;
+`;
+
+export default StyledOval;
