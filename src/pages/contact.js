e5840ed2472b904b95fd48741d3d35ae58ce6e3c
Mali Coxon <malii.coxon@gmail.com>
2020-03-25 08:45:21 +0000

    feat: Added fadein on load to rest of page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,27 +6,30 @@ import PageHeader from "../components/PageHeader";
 import BlogSnippetText from "../components/BlogSnippetText";
 import BlogLink from "../components/BlogLink";
 import OutlinedHeader from "../components/OutlinedHeader";
+import FadeInOnLoad from "../components/FadeInOnLoad/FadeInOnLoad";
 
 const Contact = () => (
     <Layout>
         <Container>
             <SEO title="Contact"/>
             <PageHeader text={"Contact"} align={"left"}/>
-            <OutlinedHeader align={"left"}>
-                {"I don't bite."}<sup>{"(much)"}</sup>
-            </OutlinedHeader>
-            <BlogSnippetText>
-                {"You can find me on Twitter "}<BlogLink to={"https://twitter.com"}>{"here."}</BlogLink>
-            </BlogSnippetText>
-            <BlogSnippetText>
-                {"You can find me on LinkedIn "}<BlogLink to={"https://twitter.com"}>{"here."}</BlogLink>
-            </BlogSnippetText>
-            <BlogSnippetText>
-                {"You can find me on GitHub "}<BlogLink to={"https://twitter.com"}>{"here."}</BlogLink>
-            </BlogSnippetText>
-            <BlogSnippetText>
-                {"You can drop me an email at "}<BlogLink to={"mail@malicoxon.co.uk"}>{"mail@malicoxon.co.uk"}</BlogLink>
-            </BlogSnippetText>
+            <FadeInOnLoad>
+                <OutlinedHeader align={"left"}>
+                    {"I don't bite."}<sup>{"(much)"}</sup>
+                </OutlinedHeader>
+                <BlogSnippetText>
+                    {"You can find me on Twitter "}<BlogLink to={"https://twitter.com"}>{"here."}</BlogLink>
+                </BlogSnippetText>
+                <BlogSnippetText>
+                    {"You can find me on LinkedIn "}<BlogLink to={"https://twitter.com"}>{"here."}</BlogLink>
+                </BlogSnippetText>
+                <BlogSnippetText>
+                    {"You can find me on GitHub "}<BlogLink to={"https://twitter.com"}>{"here."}</BlogLink>
+                </BlogSnippetText>
+                <BlogSnippetText>
+                    {"You can drop me an email at "}<BlogLink to={"mail@malicoxon.co.uk"}>{"mail@malicoxon.co.uk"}</BlogLink>
+                </BlogSnippetText>
+            </FadeInOnLoad>
         </Container>
     </Layout>
 );
