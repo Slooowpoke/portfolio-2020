522b369901f036136f19dab9bc7963825f88fbaf
Mali Coxon <malii.coxon@gmail.com>
2020-03-27 21:10:38 +0000

    fix: Fixed failing BlogSnippet test

--- src/components/BlogSnippet/BlogSnippet.test.tsx
+++ src/components/BlogSnippet/BlogSnippet.test.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { render } from "@testing-library/react";
 import { axe } from "jest-axe";
 import BlogSnippet from "./BlogSnippet";
+import InternalProvider from "gatsby-plugin-transition-link/context/InternalProvider";
 
 describe('Given a BlogSnippet component', () => {
     describe('when it is rendered', () => {
         test('it should render correctly with no a11y violations', async () => {
             const { container } = render(
-                <BlogSnippet
-                    title={"Blog Post"}
-                    to={"/blog-post"}
-                    date={"20-20-2020"}
-                    excerpt={"This is a blog snippet and a piece of text that is a snippet of the original blog post."}
-                />
+                <InternalProvider>
+                    <BlogSnippet
+                        title={"Blog Post"}
+                        to={"/blog-post"}
+                        date={"20-20-2020"}
+                        excerpt={"This is a blog snippet and a piece of text that is a snippet of the original blog"}
+                    />
+                </InternalProvider>
             );
             const results = await axe(container);
             expect(results).toHaveNoViolations()
