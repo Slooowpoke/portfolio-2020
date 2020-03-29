22f5f23b39e769eeefe8399aab69baad8ef63e80
Mali Coxon <malii.coxon@gmail.com>
2020-03-29 11:12:20 +0100

    fix: Added lang to html with html-attributes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,92 +1,92 @@
 module.exports = {
-  siteMetadata: {
-    title: `Mali Coxon`,
-    description: `Mali Coxon - Programmer`,
-    author: `@slooowpoke`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        title: `Mali Coxon`,
+        description: `Mali Coxon - Programmer`,
+        author: `@slooowpoke`,
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Portfolio 2020`,
-        short_name: `portfolio-2020`,
-        start_url: `/`,
-        background_color: `#111`,
-        theme_color: `#fffaff`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`,
-      },
-    },
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true,
-        allExtensions: true,
-      },
-    },
-    `gatsby-plugin-transition-link`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/data/projects/`,
-        name: "markdown-projects",
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              maxWidth: 590,
+                name: `images`,
+                path: `${__dirname}/src/images`,
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        path: `${__dirname}/src/data/blog/`,
-        name: "markdown-projects",
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
             options: {
-              maxWidth: 590,
+                name: `Portfolio 2020`,
+                short_name: `portfolio-2020`,
+                start_url: `/`,
+                background_color: `#111`,
+                theme_color: `#fffaff`,
+                display: `minimal-ui`,
+                icon: `src/images/icon.png`,
             },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
+        },
+        `gatsby-plugin-styled-components`,
+        {
+            resolve: `gatsby-plugin-typescript`,
             options: {
-              maxWidth: 590,
+                isTSX: true,
+                allExtensions: true,
             },
-          },
-          {
-            resolve: "gatsby-remark-embed-video",
+        },
+        `gatsby-plugin-transition-link`,
+        {
+            resolve: `gatsby-source-filesystem`,
             options: {
-              related: false,
-              noIframeBorder: true
-            }
-          },
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-responsive-iframe`
-        ],
-      },
-    }
-  ],
+                path: `${__dirname}/src/data/projects/`,
+                name: "markdown-projects",
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/src/data/blog/`,
+                name: "markdown-projects",
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 590,
+                        },
+                    },
+                    {
+                        resolve: "gatsby-remark-embed-video",
+                        options: {
+                            related: false,
+                            noIframeBorder: true
+                        }
+                    },
+                    `gatsby-remark-copy-linked-files`,
+                    `gatsby-remark-responsive-iframe`
+                ],
+            },
+        }
+    ],
 };
